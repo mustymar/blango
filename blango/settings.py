6f58936ea0611d50b6d352dc7d346906779a1b1b
Marizuk Mustapha <mustaphamarizuk98@gmail.com>
2022-11-16 05:45:45 +0000

    Finish security and passwords

--- blango/settings.py
+++ blango/settings.py
@@ -294,7 +294,7 @@ class Dev(Configuration):
         },
         "loggers": {
         "django.request": {
-            "handlers": ["mail_admins"],
+            # "handlers": ["mail_admins"],
             "level": "ERROR",
             "propagate": True,
         },
@@ -304,6 +304,14 @@ class Dev(Configuration):
         "level": "DEBUG",
         }
     }
+
+    PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
+
 
 
 class Prod(Dev):
