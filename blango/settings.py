80dd498de4af968692aa7b3d517cd5a4f7cb475b
Marizuk Mustapha <mustaphamarizuk98@gmail.com>
2022-11-18 10:55:51 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -181,9 +181,11 @@ class Dev(Configuration):
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
+        "debug_toolbar",
     ]
 
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -311,6 +313,8 @@ class Dev(Configuration):
       'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
       'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     ]
+    INTERNAL_IPS = ["192.168.10.93"]
+
 
 
 
--- blango/urls.py
+++ blango/urls.py
@@ -13,13 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+import debug_toolbar
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 
 import blog.views
+
+
 urlpatterns = [
 # other patterns
 path('admin/', admin.site.urls),
 path("", blog.views.index),
-path("post/<slug>/", blog.views.post_details, name="blog-post-detail")
+path("post/<slug>/", blog.views.post_details, name="blog-post-detail"),
+path("ip/", blog.views.get_ip)
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
