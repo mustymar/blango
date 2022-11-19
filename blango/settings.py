eb3f10e401517107f362070ac6a977d85ad87803
Marizuk Mustapha <mustaphamarizuk98@gmail.com>
2022-11-19 18:06:13 +0000

    Finish custom user model

--- blango/settings.py
+++ blango/settings.py
@@ -156,6 +156,7 @@ class Dev(Configuration):
 
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = values.BooleanValue(True)
+    AUTH_USER_MODEL = "blango_auth.user"
 
     ALLOWED_HOSTS = values.ListValue(["localhost", "0.0.0.0", ".codio.io"])
     X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
@@ -178,6 +179,7 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        'blango_auth',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
