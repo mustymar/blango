fde3c905919fba6f383dd1b48edf1c2178097d52
Marizuk Mustapha <mustaphamarizuk98@gmail.com>
2023-02-01 10:49:50 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -28,6 +28,7 @@ path(
     name="django_registration_register",
 ),
 path("accounts/", include("django_registration.backends.activation.urls")),
+path("api/v1/", include("blog.api_urls")),
 ]
 
 if settings.DEBUG:
